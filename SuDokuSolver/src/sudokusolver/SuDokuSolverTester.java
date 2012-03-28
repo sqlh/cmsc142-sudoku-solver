1e7c270d14f1a43c40b0de0be5158d5525f4c350
rickjasonobrero@gmail.com <rickjasonobrero@gmail.com@09077bd5-f45d-36d1-84a9-2f6acbedb894>
2012-03-28 06:45:36 +0000

    Switcher from the command line arguments to the main class inputs are changed.

--- SuDokuSolver/src/sudokusolver/SuDokuSolverTester.java
+++ SuDokuSolver/src/sudokusolver/SuDokuSolverTester.java
@@ -9,13 +9,21 @@ import java.io.IOException;
 public class SuDokuSolverTester {
     public static void main(String[] args) throws IllegalArgumentException, 
             IOException {
-        int generateProblem = Integer.parseInt(args[0]);
-        int problemNumber = Integer.parseInt(args[1]);
-        int SuDoku = Integer.parseInt(args[2]);
-        int SuDokuX = Integer.parseInt(args[3]);
-        int SuDokuY = Integer.parseInt(args[4]);
-        String fileIn = args[5];
-        String fileOut = args[6];
+//        int generateProblem = Integer.parseInt(args[0]);
+//        int problemNumber = Integer.parseInt(args[1]);
+//        int SuDoku = Integer.parseInt(args[2]);
+//        int SuDokuX = Integer.parseInt(args[3]);
+//        int SuDokuY = Integer.parseInt(args[4]);
+//        String fileIn = args[5];
+//        String fileOut = args[6];
+        
+        int generateProblem = 0;
+        int problemNumber = 1;
+        int SuDoku = 1;
+        int SuDokuX = 0;
+        int SuDokuY = 0;
+        String fileIn = "C:\\Users\\intel\\Documents\\puzzles\\newspaper.txt";
+        String fileOut = "C:\\Users\\intel\\Documents\\puzzles\\output.txt";
         
         switch (generateProblem){
             case 0:
